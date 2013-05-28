2ce149d53d94cbcc0dc64c76030a670d0660d08f
NiranV <NiranV.Dean@googlemail.com>
2013-05-28 15:21:44 +0200

    Changed: Double-Click "adds" objects and wearables instead of just attaching it.
    --HG--
    branch : Black Dragon

--- indra/newview/llinventorybridge.cpp
+++ indra/newview/llinventorybridge.cpp
@@ -5260,7 +5260,7 @@ void LLObjectBridge::openItem()
 {
 	// object double-click action is to wear/unwear object
 	performAction(getInventoryModel(),
-		      get_is_item_worn(mUUID) ? "detach" : "attach");
+		      get_is_item_worn(mUUID) ? "detach" : "wear_add");
 }
 
 std::string LLObjectBridge::getLabelSuffix() const
