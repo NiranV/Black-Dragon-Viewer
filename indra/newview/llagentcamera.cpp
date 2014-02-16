993a3cd4a14723389f6b2888480cec7dc4c37bae
NiranV <NiranV.Dean@googlemail.com>
2014-02-16 02:21:50 +0100

    Changed: ShowSelectionBeam now controls if our Avatar does the editing arm motion.

--- indra/newview/llagentcamera.cpp
+++ indra/newview/llagentcamera.cpp
@@ -2775,7 +2775,8 @@ void LLAgentCamera::lookAtLastChat()
 BOOL LLAgentCamera::setPointAt(EPointAtType target_type, LLViewerObject *object, LLVector3 position)
 {
 	// disallow pointing at attachments and avatars
-	if (object && (object->isAttachment() || object->isAvatar()))
+	if (object && (object->isAttachment() || object->isAvatar() || 
+		!gSavedSettings.getBOOL("ShowSelectionBeam")))
 	{
 		return FALSE;
 	}
