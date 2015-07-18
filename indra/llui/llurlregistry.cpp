6a8231f3083fe87bdbb046cdf6b42d6e8f174479
NiranV <NiranV.Dean@googlemail.com>
2015-07-18 18:25:42 +0200

    Fixed: Compile.
    --HG--
    branch : Black Dragon

--- indra/llui/llurlregistry.cpp
+++ indra/llui/llurlregistry.cpp
@@ -172,7 +172,7 @@ static bool stringHasUrl(const std::string &text)
 			text.find(".com") != std::string::npos ||
 			text.find("<nolink>") != std::string::npos ||
 			text.find("<icon") != std::string::npos ||
-			text.find("@") != std::string::npos);
+			text.find("@") != std::string::npos ||
 //			//BD
 			text.find("<red>") != std::string::npos ||
 			text.find("<green>") != std::string::npos ||
