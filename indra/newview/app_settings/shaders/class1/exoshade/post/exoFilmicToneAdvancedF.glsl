79877da9ed920f53f6a67720010166bccbee792c
NiranV <NiranV.Dean@googlemail.com>
2015-11-20 03:51:19 +0100

    Fixed: Tone Mapping not working in the correct gamma space.
    --HG--
    branch : Black Dragon

--- indra/newview/app_settings/shaders/class1/exoshade/post/exoFilmicToneAdvancedF.glsl
+++ indra/newview/app_settings/shaders/class1/exoshade/post/exoFilmicToneAdvancedF.glsl
@@ -17,6 +17,43 @@ uniform vec3 exo_advToneUA;
 uniform vec3 exo_advToneUB;
 uniform vec3 exo_advToneUC;
 
+vec3 srgb_to_linear(vec3 cs)
+{
+	vec3 low_range = cs / vec3(12.92);
+	vec3 high_range = pow((cs+vec3(0.055))/vec3(1.055), vec3(2.4));
+	bvec3 lte = lessThanEqual(cs,vec3(0.04045));
+
+#ifdef OLD_SELECT
+	vec3 result;
+	result.r = lte.r ? low_range.r : high_range.r;
+	result.g = lte.g ? low_range.g : high_range.g;
+	result.b = lte.b ? low_range.b : high_range.b;
+    return result;
+#else
+	return mix(high_range, low_range, lte);
+#endif
+
+}
+
+vec3 linear_to_srgb(vec3 cl)
+{
+	cl = clamp(cl, vec3(0), vec3(1));
+	vec3 low_range  = cl * 12.92;
+	vec3 high_range = 1.055 * pow(cl, vec3(0.41666)) - 0.055;
+	bvec3 lt = lessThan(cl,vec3(0.0031308));
+
+#ifdef OLD_SELECT
+	vec3 result;
+	result.r = lt.r ? low_range.r : high_range.r;
+	result.g = lt.g ? low_range.g : high_range.g;
+	result.b = lt.b ? low_range.b : high_range.b;
+    return result;
+#else
+	return mix(high_range, low_range, lt);
+#endif
+
+}
+
 /**
  * We setup the tone mapping settings as such:
  * A = exo_advToneUA.x
@@ -56,10 +93,11 @@ void main ()
 	float ExposureBias = exo_advToneUC.y;
 
 	vec4 diff = texture2DRect(exo_screen, vary_fragcoord.xy);
+	diff.rgb = srgb_to_linear(diff.rgb);
 	diff.rgb *= exo_exposure;
 	
    	vec3 curr = Uncharted2Tonemap(ExposureBias* diff.rgb);
 	vec3 whiteScale = vec3(1.0)/Uncharted2Tonemap(vec3(W));
 	vec3 color = curr*whiteScale;
-	frag_color = vec4(color, diff.a);
+	frag_color = vec4(linear_to_srgb(color), diff.a);
 }
--- indra/newview/app_settings/shaders/class1/exoshade/post/exoFilmicToneF.glsl
+++ indra/newview/app_settings/shaders/class1/exoshade/post/exoFilmicToneF.glsl
@@ -15,9 +15,28 @@ uniform sampler2DRect exo_screen;
 uniform float exo_exposure;
 VARYING vec2 vary_fragcoord;
 
+vec3 srgb_to_linear(vec3 cs)
+{
+	vec3 low_range = cs / vec3(12.92);
+	vec3 high_range = pow((cs+vec3(0.055))/vec3(1.055), vec3(2.4));
+	bvec3 lte = lessThanEqual(cs,vec3(0.04045));
+
+#ifdef OLD_SELECT
+	vec3 result;
+	result.r = lte.r ? low_range.r : high_range.r;
+	result.g = lte.g ? low_range.g : high_range.g;
+	result.b = lte.b ? low_range.b : high_range.b;
+    return result;
+#else
+	return mix(high_range, low_range, lte);
+#endif
+
+}
+
 void main ()
 {
 	vec4 diff = texture2DRect(exo_screen, vary_fragcoord.xy);
+	diff.rgb = srgb_to_linear(diff.rgb);
 	diff.rgb *= exo_exposure;
    	vec3 x = max(vec3(0), diff.rgb-0.004);
    	diff.rgb = (x*(6.2*x+.5))/(x*(6.2*x+1.7)+0.06);
