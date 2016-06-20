42c8a0a60b3cf7865f7a21a7f725e314853e7e22
NiranV <NiranV.Dean@googlemail.com>
2016-06-20 09:54:02 +0200

    Fixed: Allow Mouselook Grab option not working.
    --HG--
    branch : Black Dragon

--- indra/newview/lltoolgrab.cpp
+++ indra/newview/lltoolgrab.cpp
@@ -143,15 +143,20 @@ BOOL LLToolGrab::handleMouseDown(S32 x, S32 y, MASK mask)
 		LL_INFOS() << "LLToolGrab handleMouseDown" << LL_ENDL;
 	}
 
-	// call the base class to propogate info to sim
-	LLTool::handleMouseDown(x, y, mask);
-	
-	if (!gAgent.leftButtonBlocked())
+//	//BD - Allow Mousellok Grab
+	if ((gAgentCamera.cameraMouselook()
+		&& gSavedSettings.getBOOL("AllowMouselookGrab")))
 	{
-		// can grab transparent objects (how touch event propagates, scripters rely on this)
-		gViewerWindow->pickAsync(x, y, mask, pickCallback, /*BOOL pick_transparent*/ TRUE);
+		// call the base class to propogate info to sim
+		LLTool::handleMouseDown(x, y, mask);
+
+		if (!gAgent.leftButtonBlocked())
+		{
+			// can grab transparent objects (how touch event propagates, scripters rely on this)
+			gViewerWindow->pickAsync(x, y, mask, pickCallback, /*BOOL pick_transparent*/ TRUE);
+		}
+		mClickedInMouselook = gAgentCamera.cameraMouselook();
 	}
-	mClickedInMouselook = gAgentCamera.cameraMouselook();
 	return TRUE;
 }
 
