c88ccca274474750820854d71dc78aa69d2ee54a
NiranV <NiranV.Dean@googlemail.com>
2017-01-09 18:18:25 +0100

    Changed: Tweaks to SSAO smoothing.
    --HG--
    branch : Black Dragon

--- indra/newview/app_settings/shaders/class2/deferred/sunLightSSAOF.glsl
+++ indra/newview/app_settings/shaders/class2/deferred/sunLightSSAOF.glsl
@@ -119,7 +119,7 @@ float calcAmbientOcclusion(vec4 pos, vec3 norm)
 
 	vec2 pos_screen = vary_fragcoord.xy;
 	vec3 pos_world = pos.xyz;
-	vec2 noise_reflect = texture2D(noiseMap, vary_fragcoord.xy/128.0).xy;
+	vec2 noise_reflect = texture2D(noiseMap, vary_fragcoord.xy/64.0).xy;
 		
 	float angle_hidden = 0.0;
 	float points = 0;
