be382665e4de9b95669d516c3c8f38f7fd18bf53
NiranV <NiranV.Dean@googlemail.com>
2017-02-20 02:04:06 +0100

    Changed: Enable projector shadow toggle only if the light is a projector.
    --HG--
    branch : Black Dragon

--- indra/newview/llpanelvolume.cpp
+++ indra/newview/llpanelvolume.cpp
@@ -287,7 +287,10 @@ void LLPanelVolume::getState( )
 	//BD
 	BOOL has_shadow = volobjp && volobjp->getHasShadow();
 	getChild<LLUICtrl>("Shadow Checkbox Ctrl")->setValue(has_shadow);
-	getChildView("Shadow Checkbox Ctrl")->setEnabled(editable && single_volume && volobjp);
+	getChildView("Shadow Checkbox Ctrl")->setEnabled(editable 
+													&& single_volume 
+													&& volobjp 
+													&& volobjp->getLightTextureID().notNull());
 	
 	if (is_light && editable && single_volume)
 	{
