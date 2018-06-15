0c701f6e7ae31799774128b576229b6de78900b6
NiranV <NiranV.Dean@googlemail.com>
2018-06-16 00:01:20 +0200

    Misc: Changes to the copyright text in my code files.
    --HG--
    branch : Black Dragon

--- indra/newview/bdfloatercamera.h
+++ indra/newview/bdfloatercamera.h
@@ -1,6 +1,6 @@
 /**
 *
-* Copyright (C) 2017, NiranV Dean
+* Copyright (C) 2018, NiranV Dean
 *
 * This library is free software; you can redistribute it and/or
 * modify it under the terms of the GNU Lesser General Public
