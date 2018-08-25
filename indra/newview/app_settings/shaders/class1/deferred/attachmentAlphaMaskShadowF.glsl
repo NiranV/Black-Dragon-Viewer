d0fd831572a2e2b881108577a3519a2a59344c69
NiranV <NiranV.Dean@googlemail.com>
2018-08-25 15:55:17 +0200

    Fixed: Shadows from rigged alpha masked/blended surfaces being skipped.
    --HG--
    branch : Black Dragon

--- indra/newview/app_settings/shaders/class1/deferred/attachmentAlphaMaskShadowF.glsl
+++ indra/newview/app_settings/shaders/class1/deferred/attachmentAlphaMaskShadowF.glsl
@@ -41,7 +41,7 @@ VARYING vec4 vertex_color;
 
 void main() 
 {
-	float alpha = texture2D(diffuseMap, vary_texcoord0.xy).a * vertex_color.a;
+	float alpha = texture2D(diffuseMap, vary_texcoord0.xy).a ;
 
 	if (alpha < 0.05) // treat as totally transparent
 	{
