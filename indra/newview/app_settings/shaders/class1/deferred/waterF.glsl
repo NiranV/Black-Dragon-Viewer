2ba7bfcf66b1c4ca9b9c24cb5cb3ec831d4e65e0
NiranV <NiranV.Dean@googlemail.com>
2018-09-22 07:30:58 +0200

    Fixed: Water color mismatch between void water and SIM water.
    --HG--
    branch : Black Dragon

--- indra/newview/app_settings/shaders/class1/deferred/waterF.glsl
+++ indra/newview/app_settings/shaders/class1/deferred/waterF.glsl
@@ -189,7 +189,7 @@ void main()
 	
 	color.rgb += spec * specular;
 	
-	color.rgb = atmosTransport(color.rgb);
+	//color.rgb = atmosTransport(color.rgb);
 	color.rgb = scaleSoftClip(color.rgb);
 	color.a   = spec * sunAngle2;
 
