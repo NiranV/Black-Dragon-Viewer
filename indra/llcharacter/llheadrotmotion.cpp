5f3cae744c81e58a6ba89b5b41b32961e30fb530
NiranV <NiranV.Dean@googlemail.com>
2019-10-07 04:44:09 +0200

    Fixed: (Again) Heads of certain avatars looking straight forward relative to their body instead of looking around properly.
    --HG--
    branch : Black Dragon

--- indra/llcharacter/llheadrotmotion.cpp
+++ indra/llcharacter/llheadrotmotion.cpp
@@ -247,8 +247,8 @@ BOOL LLHeadRotMotion::onUpdate(F32 time, U8* joint_mask)
 	// Set the head rotation.
 	if (mNeckState->getJoint() && mNeckState->getJoint()->getParent())
 	{
-		//LLQuaternion torsoRotLocal = mNeckState->getJoint()->getParent()->getWorldRotation() * currentInvRootRotWorld;
-		//head_rot_local = head_rot_local * currentRootRotWorld;
+		LLQuaternion torsoRotLocal = mNeckState->getJoint()->getParent()->getWorldRotation() * currentInvRootRotWorld;
+		head_rot_local = head_rot_local * ~torsoRotLocal;
 		mNeckState->setRotation(nlerp(NECK_LAG, LLQuaternion::DEFAULT, head_rot_local));
 		mHeadState->setRotation(nlerp(1.f - NECK_LAG, LLQuaternion::DEFAULT, head_rot_local));
 	}
