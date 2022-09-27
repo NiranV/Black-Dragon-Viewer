fc5c2446c9cd9b5128bb8942fb6e199e4f37b152
NiranV <niranv.dean@googlemail.com>
2022-09-27 18:46:05 +0200

    Fixed: Crash when using RLVa.

--- indra/newview/rlvmodifiers.h
+++ indra/newview/rlvmodifiers.h
@@ -171,7 +171,7 @@ class RlvCachedBehaviourModifier
 public:
 	RlvCachedBehaviourModifier(ERlvBehaviourModifier eModifier)
 	{
-		if (RlvBehaviourModifierCache<T>::getInstance(eModifier) == nullptr)
+		if ((mCachedModifierPtr = RlvBehaviourModifierCache<T>::getInstance(eModifier).get()) == nullptr)
 			mCachedModifierPtr = new RlvBehaviourModifierCache<T>(eModifier);
 	}
 
