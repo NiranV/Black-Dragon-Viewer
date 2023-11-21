e6c092637b8aa3b9de338e284479832653807c8d
NiranV <niranv.dean@googlemail.com>
2023-11-21 16:39:40 +0100

    Misc: Compile fixes.

--- indra/newview/lloutfitslist.h
+++ indra/newview/lloutfitslist.h
@@ -240,6 +240,11 @@ public:
 	 */
     /*virtual*/ bool hasItemSelected();
 
+    /**
+     * BD - Returns true if all selected items can be worn.
+     */
+    bool canWearSelected();
+
 	/**
 	Collapses all outfit accordions.
 	*/
@@ -311,11 +316,6 @@ private:
 	 * @see applyFilter()
 	 */
 	void applyFilterToTab(const LLUUID& category_id, LLAccordionCtrlTab* tab, const std::string& filter_substring);
-
-	/**
-	 * Returns true if all selected items can be worn.
-	 */
-	bool canWearSelected();
 
 	void onWearableItemsListRightClick(LLUICtrl* ctrl, S32 x, S32 y);
 	void onCOFChanged();
