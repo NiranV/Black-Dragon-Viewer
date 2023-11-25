b9b568a4214fe89d4054b5d1fc7c6d8c044559af
NiranV <niranv.dean@googlemail.com>
2023-11-25 23:02:48 +0100

    Fixed: Added Missing "Create New" menu in right-click Inventory. Fixed: Added Missing create "Material" menu entry in Inventory.

--- indra/newview/llinventorybridge.cpp
+++ indra/newview/llinventorybridge.cpp
@@ -765,7 +765,7 @@ void hide_context_entries(LLMenuGL& menu,
 
 		// descend into split menus:
 		LLMenuItemBranchGL* branchp = dynamic_cast<LLMenuItemBranchGL*>(menu_item);
-		if ((name == "More") && branchp)
+		if (((name == "More") || (name == "create_new")) && branchp)
 		{
 			hide_context_entries(*branchp->getBranch(), entries_to_show, disabled_entries);
 		}
@@ -4282,6 +4282,7 @@ void LLFolderBridge::buildContextMenuOptions(U32 flags, menuentry_vec_t&   items
 
 		disabled_items.push_back(std::string("New Folder"));
 		disabled_items.push_back(std::string("upload_def"));
+		disabled_items.push_back(std::string("create_new"));
 	}
 	if (favorites == mUUID)
 	{
@@ -4304,6 +4305,7 @@ void LLFolderBridge::buildContextMenuOptions(U32 flags, menuentry_vec_t&   items
     {
         disabled_items.push_back(std::string("New Folder"));
 		disabled_items.push_back(std::string("upload_def"));
+		disabled_items.push_back(std::string("create_new"));
     }
     if (marketplace_listings_id == mUUID)
     {
@@ -4366,7 +4368,19 @@ void LLFolderBridge::buildContextMenuOptions(U32 flags, menuentry_vec_t&   items
 				}
                 if (!isMarketplaceListingsFolder())
                 {
-                    items.push_back(std::string("upload_def"));
+					items.push_back(std::string("upload_def"));
+					items.push_back(std::string("create_new"));
+					items.push_back(std::string("New Script"));
+					items.push_back(std::string("New Note"));
+					items.push_back(std::string("New Gesture"));
+					items.push_back(std::string("New Material"));
+					items.push_back(std::string("New Clothes"));
+					items.push_back(std::string("New Body Parts"));
+					items.push_back(std::string("New Settings"));
+					if (!LLEnvironment::instance().isInventoryEnabled())
+					{
+						disabled_items.push_back("New Settings");
+					}
                 }
                 if (menu_items_added)
                 {
